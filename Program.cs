e3e835f378ef23905adf13e58024b4b8237759bd
gaiyaobed <95748043+gaiyaobed@users.noreply.github.com>
2023-10-26 00:45:03 +0100

    ff

--- Program.cs
+++ Program.cs
@@ -175,11 +175,14 @@ builder.Services.AddValidatorsFromAssemblyContaining<ResetPasswordDtoValidator>(
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseForwardedHeaders(new ForwardedHeadersOptions
 {
